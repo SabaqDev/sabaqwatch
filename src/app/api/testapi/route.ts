9b1db2ba9fe14894f67f8a19134aa71c0903f0b5
ReinhyVW <manu_cabarosa32@outlook.com>
2023-11-03 17:01:23 -0500

    Checking

--- src/app/api/testapi/route.ts
+++ src/app/api/testapi/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export default function GET() {
+export async function GET() {
   const data = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
   return NextResponse.json(data);
